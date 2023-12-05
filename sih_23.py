f28ee37f86f106d497b8a0ab6593711f1204d0ab
VARUN-688 <110732585+VARUN-688@users.noreply.github.com>
2023-12-05 20:07:08 +0530

    Update sih_23.py

--- sih_23.py
+++ sih_23.py
@@ -5,18 +5,27 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 driver=webdriver.Chrome()
-driver.get("https://www.sih.gov.in/sih2023-screening-result-batch5")
-driver.maximize_window()
-table=driver.find_element(By.XPATH,'//*[@id="sheet0"]')
+i=[1,2,3,4,5]
+try:
+  bt=int(intput("Enter the batch number of result :-> "))
 
-table_html = table.get_attribute('outerHTML')
-# Convert the HTML table to a Pandas DataFrame
-df = pd.read_html(table_html)[0]
-
-# Save the DataFrame to an Excel file
-df.to_excel('batch-5.xlsx', index=False)
-
-# Close the browser window
-driver.quit()
-
-print('Table copied to Excel successfully using Selenium.')
+except:
+  print("Wrong input or something went wrong")
+else:
+    driver.get(f"https://www.sih.gov.in/sih2023-screening-result-batch{i[bt]}")
+    driver.maximize_window()
+    table=driver.find_element(By.XPATH,'//*[@id="sheet0"]')
+    
+    table_html = table.get_attribute('outerHTML')
+    # Convert the HTML table to a Pandas DataFrame
+    df = pd.read_html(table_html)[0]
+    
+    # Save the DataFrame to an Excel file
+    df.to_excel(f'batch-{i[bt]}.xlsx', index=False)
+    
+    # Close the browser window
+    driver.quit()
+    
+    print('Table copied to Excel successfully using Selenium.')
+finally:
+  print("Thank for using the code")
